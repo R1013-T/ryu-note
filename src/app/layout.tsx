e0b4179a9f3d2911f64982e2ad9e8de1a19f3a0d
R1013-T <takahashi.ryunosuke.job@gmail.com>
2025-02-15 14:54:37 +0900

    🚀

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Providers } from '~/common/providers'
-import '~/app/globals.module.css'
+import '~/globals.css'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
